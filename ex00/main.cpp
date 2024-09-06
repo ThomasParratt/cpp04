27de54bb6ac7ef05933a04017dd01ef9ae875863
root <root@LAPTOP-J70A2AVE>
2024-09-06 15:22:11 +0300

    ex00

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -4,17 +4,32 @@
 
 int main(void)
 {
-    const Animal* meta = new Animal();
-    const Animal* j = new Dog();
-    const Animal* i = new Cat();
-    //const WrongAnimal* i = new WrongCat();
-    std::cout << j->getType() << " " << std::endl;
-    std::cout << i->getType() << " " << std::endl;
-    i->makeSound(); //will output the cat sound!
-    j->makeSound();
-    meta->makeSound();
-    delete meta;
-    delete j;
-    delete i;
+    {
+        const Animal* meta = new Animal();
+        const Animal* j = new Dog();
+        const Animal* i = new Cat();
+        std::cout << j->getType() << " " << std::endl;
+        std::cout << i->getType() << " " << std::endl;
+        i->makeSound(); //will output the cat sound!
+        j->makeSound();
+        meta->makeSound();
+        delete meta;
+        delete j;
+        delete i;
+    }
+    std::cout << std::endl;
+    {
+        const Animal* meta = new Animal();
+        const Animal* j = new Dog();
+        const WrongAnimal* i = new WrongCat();
+        std::cout << j->getType() << " " << std::endl;
+        std::cout << i->getType() << " " << std::endl;
+        i->makeSound(); //will output the cat sound!
+        j->makeSound();
+        meta->makeSound();
+        delete meta;
+        delete j;
+        delete i;
+    }
     return (0);
 }
