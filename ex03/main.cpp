ea17b2b0696ea0c32bfa2562ff00b10748ade143
Thomas Parratt <tparratt@c2r5p6.hive.fi>
2024-09-16 15:57:30 +0300

    f

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -17,7 +17,7 @@ int main(void)
     AMateria* tmp;
     tmp = src->createMateria("ice");
     std::cout << "tmp type = " << tmp->getType() << std::endl;
-    me->equip(tmp); //equip takes ownership of tmp and manages it's lifetime
+    me->equip(tmp);
     tmp = src->createMateria("cure");
     std::cout << "tmp type = " << tmp->getType() << std::endl;
     me->equip(tmp);
@@ -37,44 +37,3 @@ int main(void)
 
     return (0);
 }
-
-// int main(void)
-// {
-//     AMateria    *materia_1 = new Ice();
-//     AMateria    *materia_2 = materia_1->clone();
-//     AMateria    *materia_3 = new Cure();
-//     AMateria    *materia_4 = new Cure();
-//     std::cout << "materia_1 is " << materia_1->getType() << std::endl;
-//     std::cout << "materia_2 is " << materia_2->getType() << std::endl;
-//     std::cout << "materia_3 is " << materia_3->getType() << std::endl;
-//     std::cout << "materia_4 is " << materia_4->getType() << std::endl;
-
-//     Character  *Tom = new Character("Thomas");
-//     Character  *Sam = new Character("Samuel");
-//     materia_1->use(*Tom); // the argument in use should be ICharacter?
-//     materia_3->use(*Tom);
-
-//     Tom->equip(materia_1);
-//     Tom->print_slot(0);
-//     Tom->equip(materia_2);
-//     Tom->print_slot(1);
-//     Tom->equip(materia_3);
-//     Tom->print_slot(2);
-//     Tom->equip(materia_4); // this does nothing because all slots are full
-//     Tom->print_slot(0);
-
-//     Tom->unequip(0);
-//     Tom->print_slot(0);
-//     Tom->equip(materia_1);
-//     Tom->print_slot(0);
-
-//     Tom->use(0, *Sam);
-//     Tom->use(2, *Sam);
-
-//     delete materia_1;
-//     delete materia_2;
-//     delete materia_3;
-//     delete materia_4;
-//     delete Tom;
-//     delete Sam;
-// }
